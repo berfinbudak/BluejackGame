cf136eff5c0cae1f30fa8451ac8105ef18a14efa
berfinbudak <adabbudak@gmail.com>
2023-12-19 13:47:30 +0300

    calculating winner

--- BluejackGame.java
+++ BluejackGame.java
@@ -9,7 +9,7 @@ public class BluejackGame {
     private static Card[] gameDeck = new Card[DECK_SIZE];
     private static Card[] computerDeck = new Card[BOARD_SIZE];
     private static Card[] userDeck = new Card[BOARD_SIZE];
-    
+
     private static Card[] computerHand = new Card[HAND_SIZE];
     private static Card[] userHand = new Card[HAND_SIZE];
 
@@ -27,7 +27,7 @@ public class BluejackGame {
         dealDecks();
 
         System.out.println("\nStart of BlueJack");
-        
+
         System.out.println("\n--------------------\n");
 
         System.out.println("Computer Deck");
@@ -40,10 +40,11 @@ public class BluejackGame {
         System.out.println("\n--------------------\n");
 
         playRound();
-        
+
         System.out.println("\n--------------------\n");
         System.out.println("Game Over");
         showScores();
+        calculateWinner();
     }
 
     private static void initializeGameDeck() {
@@ -91,7 +92,7 @@ public class BluejackGame {
                 card2.setSign("-");
             } else {
                 card2.setSign("+");
-            }           
+            }
             userDeck[i] = card2;
         }
 
@@ -100,10 +101,19 @@ public class BluejackGame {
             if ( chance <= 80 ) {
                 computerDeck[i] = gameDeck[i];
                 computerDeck[i+1] = gameDeck[i+1];
-            } 
+            }
             else {
-                computerDeck[i] = new Card("", 0, "x2");
-                computerDeck[i+1] = new Card("", 0, "x2");
+                chance = random.nextInt(2);
+                if (chance == 1)
+                    computerDeck[i] = new Card("", 0, "x2");
+                else
+                    computerDeck[i] = new Card("", 0, "+/-");
+
+                chance = random.nextInt(2);
+                if (chance == 1)
+                    computerDeck[i+1] = new Card("", 0, "x2");
+                else
+                    computerDeck[i+1] = new Card("", 0, "+/-");
             }
         }
 
@@ -114,10 +124,19 @@ public class BluejackGame {
             if ( chance <= 80 ) {
                 userDeck[i] = gameDeck[i];
                 userDeck[i+1] = gameDeck[i+1];
-            } 
+            }
             else {
-                userDeck[i] = new Card("", 0, "x2");
-                userDeck[i+1] = new Card("", 0, "x2");
+                chance = random.nextInt(2);
+                if (chance == 1)
+                    computerDeck[i] = new Card("", 0, "x2");
+                else
+                    computerDeck[i] = new Card("", 0, "+/-");
+
+                chance = random.nextInt(2);
+                if (chance == 1)
+                    computerDeck[i+1] = new Card("", 0, "x2");
+                else
+                    computerDeck[i+1] = new Card("", 0, "+/-");
             }
         }
     }
@@ -134,35 +153,36 @@ public class BluejackGame {
     private static void printDeck(Card[] deck) {
         int n=1;
         for (Card card : deck) {
-                System.out.print(n + "- ");
-                System.out.println(card);
-                n++;
+            System.out.print(n + "- ");
+            System.out.println(card);
+            n++;
         }
     }
 
-    private static void playCard(int cardIndex, Card[] hand, Card[] board) {
+    private static Card[] playCard(int cardIndex, Card[] hand, Card[] board) {
         int arrayIndex = cardIndex - 1;
-    
+
         for (int i = 0; i < 9; i++) {
             if (board[i] == null) {
-                board[i] = hand[arrayIndex];
-                break;
+
+                if (arrayIndex >= 0 && arrayIndex < hand.length) {
+                    board[i] = hand[arrayIndex];
+
+                    for (int j = arrayIndex; j < hand.length - 1; j++) {
+                        hand[j] = hand[j + 1];
+                    }
+
+                    Card[] newHand = new Card[hand.length - 1];
+                    System.arraycopy(hand, 0, newHand, 0, newHand.length);
+
+                    return newHand;
+                }
             }
         }
-    
-        for (int i = arrayIndex; i < hand.length - 1; i++) {
-            hand[i] = hand[i + 1];
-        }
-    
-        Card[] newHand = new Card[hand.length-1];
 
-        for ( int i = 0; i < hand.length-1; i++){
-            newHand[i] = hand[i];
-        }
-
-        hand = newHand;
-
+        return hand;
     }
+
 
     private static Card randomCard(){
         int chance = random.nextInt(DECK_SIZE);
@@ -171,29 +191,57 @@ public class BluejackGame {
     }
 
     private static boolean isGameOver(){
-        if (userScore >= 20 || computerScore >= 20 )
+        if (userScore >= 20 || computerScore >= 20 || userHand.length<=0 || computerHand.length<=0)
             return true;
-        else 
+        else
             return false;
     }
 
-    private static void calculateScores(){
+    private static void calculateScores() {
+        userScore = 0;
+        computerScore = 0;
+
         for (int i = 0; i < userBoard.length; i++) {
             if (userBoard[i] != null) {
-                userScore += userBoard[i].getValue();
-            }
-            else 
+                int value = userBoard[i].getValue();
+                String sign = userBoard[i].getSign();
+
+                if ("-".equals(sign)) {
+                    value = -value;
+                    userScore += value;
+                } else if ("x2".equals(sign)) {
+                    userScore = userScore*2;
+                } else if ("+/-".equals(sign))
+                    userScore = -userScore;
+                else
+                    userScore += value;
+
+            } else {
                 break;
+            }
         }
 
         for (int i = 0; i < computerBoard.length; i++) {
             if (computerBoard[i] != null) {
-                computerScore += computerBoard[i].getValue();
-            }
-            else 
+                int value = computerBoard[i].getValue();
+                String sign = computerBoard[i].getSign();
+
+                if ("-".equals(sign)) {
+                    value = -value;
+                    computerScore += value;
+                } else if ("x2".equals(sign)) {
+                    computerScore = computerScore*2;
+                } else if ("+/-".equals(sign))
+                    computerScore = -computerScore;
+                else
+                    computerScore += value;
+
+            } else {
                 break;
+            }
         }
     }
+
 
     private static void showScores(){
         calculateScores();
@@ -202,24 +250,25 @@ public class BluejackGame {
     }
 
     private static void playRound(){
-        
+
         Scanner scan = new Scanner( System.in);
         int selection;
-        
-        // CONSTANTS
+
         int MENU_EXIT = 0;
         int MENU_PLAY_1 = 1;
         int MENU_PLAY_2 = 2;
         int MENU_PLAY_3 = 3;
         int MENU_PLAY_4 = 4;
         int MENU_PLAY_RANDOM  = 5;
-        int MENU_SCORES = 6;
+        int MENU_END = 6;
+        int MENU_STAND = 7;
+        int MENU_SCORES = 8;
 
-        // display menu, get and process selection, until exit
         do
         {
-            
-            System.out.println("\n--------------------\n");
+
+            if (isGameOver())
+                break;
 
             System.out.println("\nComputer Hand:");
             printDeck(computerHand);
@@ -230,54 +279,82 @@ public class BluejackGame {
 
             // display menu
             System.out.println();
-            
+
             System.out.println();
-            System.out.println( MENU_PLAY_1 + " - to play card 1"  );
-            System.out.println( MENU_PLAY_2 + " - to play card 2"  );
-            System.out.println( MENU_PLAY_3 + " - to play card 3"  );
-            System.out.println( MENU_PLAY_4 + " - to play card 4" );
+
+            for (int i = 1; i <= userHand.length; i++) {
+                System.out.println( i + " - to play card " + i  );
+            }
             System.out.println( MENU_PLAY_RANDOM + " - to play the random card (" + card + ")" );
+            System.out.println( MENU_END + " - to end turn" );
+            System.out.println( MENU_END + " - to stand" );
             System.out.println( MENU_SCORES + " - for Scores" );
-            
+
             // ask for and get selection
             System.out.println();
             System.out.println( "Selection (" + MENU_EXIT + " to exit): ");
             selection = scan.nextInt();
-            
+
             // process selection
-            if ( selection == MENU_PLAY_1 ){
-                playCard(1, userHand, userBoard);
+            if ( selection == MENU_PLAY_1 && userHand.length>=1){
+                userHand = playCard(1, userHand, userBoard);
                 playCard(random.nextInt(BOARD_SIZE), computerHand, computerBoard);
                 showScores();
             }
-            else if ( selection == MENU_PLAY_2 ){
-                playCard(2, userHand, userBoard);
+            else if ( selection == MENU_PLAY_2  && userHand.length>=2 ){
+                userHand = playCard(2, userHand, userBoard);
                 playCard(random.nextInt(BOARD_SIZE), computerHand, computerBoard);
                 showScores();
             }
-            else if ( selection == MENU_PLAY_3 ){
-                playCard(3, userHand, userBoard);
+            else if ( selection == MENU_PLAY_3  && userHand.length>=3 ){
+                userHand = playCard(3, userHand, userBoard);
                 playCard(random.nextInt(BOARD_SIZE), computerHand, computerBoard);
                 showScores();
             }
-            else if ( selection == MENU_PLAY_4 ){
-                playCard(4, userHand, userBoard);
+            else if ( selection == MENU_PLAY_4  && userHand.length>=4 ){
+                userHand = playCard(4, userHand, userBoard);
                 playCard(random.nextInt(BOARD_SIZE), computerHand, computerBoard);
                 showScores();
             }
             else if ( selection == MENU_PLAY_RANDOM ){
-                playCard(5, userHand, userBoard);
+                userHand = playCard(5, userHand, userBoard);
                 playCard(random.nextInt(BOARD_SIZE), computerHand, computerBoard);
                 showScores();
             }
+            /* else if ( selection == MENU_END){
+                
+            }
+            else if ( selection == MENU_STAND ){
+                
+            } handle these */
             else if ( selection == MENU_SCORES )
                 showScores();
 
             else if ( selection == MENU_EXIT )
                 break;
-            
-        } while ( selection != MENU_EXIT || !isGameOver() ); 
+            else
+                System.out.println("Unvalid choice, please try again");
+
+
+            System.out.println("\n--------------------\n");
+
+        } while ( selection != MENU_EXIT || !isGameOver() );
 
         return;
+    }
+
+    private static void calculateWinner(){
+        if ( userScore >= 20 )
+            System.out.println("Computer won! You exceeded 20");
+        else if (computerScore >= 20)
+            System.out.println("You won! Computer exceeded 20");
+        else if (computerScore >= 20 && userScore >= 20 )
+            System.out.println("Tie! You both exceeded 20 at the same round");
+        else if (Math.abs(20 - userScore) > Math.abs(20 - computerScore))
+            System.out.println("Computer won! It's score was closer to 20");
+        else if (Math.abs(20 - userScore) < Math.abs(20 - computerScore))
+            System.out.println("You won! Your score was closer to 20");
+        else
+            System.out.println("There was an unexpected case");
     }
 }
